19d5fde35afb08e457a65d407e08acf571acb966
reyabs17 <114021780+reyabs17@users.noreply.github.com>
2023-09-20 20:39:35 -0400

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Reyab Saluja on 2023-09-20.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
